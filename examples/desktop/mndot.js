5eb49b10c02ba685847ec3c794b6d36cc7fbaf37
Dan 'Ducky' Little <theduckylittle@gmail.com>
2017-05-24 07:37:10 -0500

    MnDOT "additional details" example branch.

---
+++ examples/desktop/mndot.js
@@ -0,0 +1,8 @@
+mndot = {
+
+    moreInfo: function(pin) {
+        var more_info_div = document.getElementById(pin+'-more-info');
+        
+        more_info_div.innerHTML = 'More info on: '+pin;
+    }
+};
