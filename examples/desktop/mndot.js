4a9cd41c97680b9d24865cdf6a7a3c08eeffde46
Dan 'Ducky' Little <theduckylittle@gmail.com>
2017-05-24 07:49:11 -0500

    More async identify loading data example.

--- examples/desktop/mndot.js
+++ examples/desktop/mndot.js
@@ -2,7 +2,20 @@ mndot = {
 
     moreInfo: function(pin) {
         var more_info_div = document.getElementById(pin+'-more-info');
-        
-        more_info_div.innerHTML = 'More info on: '+pin;
+        more_info_div.innerHTML = 'Loading...';
+
+        // as this returns "instantly", I added a delay 
+        //  to demo the "loading..." actually working.
+        setTimeout(function() {
+            gm3.util.xhr({
+                method: 'GET',
+                url: './index.html',
+                data: {
+                    pin: pin
+                }
+            }).then(function(response) {
+                more_info_div.innerHTML = 'More info about: '+pin;
+            });
+        }, 2000);
     }
 };
